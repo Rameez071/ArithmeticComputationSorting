eeb5bf3a5aca37f451ae7fae2dc15971afe19f3c
Rameez071 <rameezraja405@gmail.com>
2020-09-27 10:49:00 +0530

    Refactor : Sort into Ascending

--- arithmeticCompSort.sh
+++ arithmeticCompSort.sh
@@ -39,3 +39,50 @@ result[$((count++))]="$( arthematic_Compute_fourth $(($val1,$val2,$val3)) )"
 
 echo "${result[@]}"
 echo "${!result[@]}"
+
+#Sorting in Descending order
+for (( i=0; i<4; i++ ))
+do
+     arr[i]=${result[$i]}
+     echo "${arr[i]}"
+done
+
+function sorting_Array_inDecending_order()
+{
+   for (( i=0; i<4; i++ ))
+   do
+      for (( j=$i+1; j<4; j++ ))
+      do
+         if [[ ${arr[i]} -lt ${arr[j]} ]] 
+         then
+              temp=${arr[i]}
+              arr[i]=${arr[j]}
+              arr[j]=$temp
+         fi
+      done
+   done
+   echo ${arr[@]}
+}
+
+res="$( sorting_Array_inDecending_order ${arr[@]} )"
+echo "decending order is : $res"
+
+#Sorting in Ascending order
+function sorting_Array_inAscending_order()
+{
+   for (( i=0; i<4; i++ ))
+   do
+      for (( j=$i+1; j<4; j++ ))
+      do
+         if [[ ${arr[i]} -gt ${arr[j]} ]]
+         then                                                                                                                                                                                                                    temp=${arr[i]}
+              arr[i]=${arr[j]}
+              arr[j]=$temp
+         fi
+      done
+   done
+              echo ${arr[@]}
+}
+
+res1="$( sorting_Array_inAscending_order ${arr[@]} )"
+echo "Ascending order is : $res1"
