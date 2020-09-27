a18647e8b2529d9c3e1c32a3a1bcbd165fd142a9
Rameez071 <rameezraja405@gmail.com>
2020-09-27 10:29:45 +0530

    Add : 4th compute added

--- arithmeticCompSort.sh
+++ arithmeticCompSort.sh
@@ -29,3 +29,9 @@ function arthematic_Compute_third()
 
 result[$((count++))]="$( arthematic_Compute_third $(($val1,$val2,$val3)) )"
 }
+
+#4th compute
+function arthematic_Compute_fourth()
+{
+  echo "$(( $val1%$val2+$val3 ))"
+}
