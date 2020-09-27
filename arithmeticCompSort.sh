45ea4d2c1609fe53b1c2cf7bcc68b6257d110eed
Rameez071 <rameezraja405@gmail.com>
2020-09-27 10:09:20 +0530

    Add : 1st compute added

--- arithmeticCompSort.sh
+++ arithmeticCompSort.sh
@@ -5,12 +5,12 @@ read -p "enter the 1st no:" val1
 read -p "enter the 2nd no:" val2
 read -p "enter the 3rd no:" val3
 
-#function for 1st compute
-function arithmetic_Compute()
+#1st compute 
+function arthematic_Compute()
 {
   echo "$(( $val1+$val2*$val3 ))"
 }
 
-result[$((count++))]="$( arithmetic_Compute $(($val1,$val2,$val3)) )"
+result[$((count++))]="$( arthematic_Compute $(($val1,$val2,$val3)) )"
 
 
